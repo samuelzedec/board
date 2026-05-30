d2fe821254f9e78a0560bd4f9672e4d1effd9cdf
Samuel Zedec <samuel.ribeiro77f@gmail.com>
2026-05-30 15:55:57 -0400

    fix: ajusta o GlobalExceptionHandler para melhorar o retorno dos erros

--- src/Board.Api/Middlewares/GlobalExceptionHandler.cs
+++ src/Board.Api/Middlewares/GlobalExceptionHandler.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Board.Application.Exceptions;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
@@ -17,28 +18,32 @@ internal sealed class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> log
         logger.LogError(exception, "Exception occurred: {ExceptionType} - {Message}",
             exception.GetType().Name, exception.Message);
 
-        var problemDetails = GetProblemDetails(httpContext, exception);
+        var problemDetails = GetProblemDetails(exception);
         httpContext.Response.StatusCode = problemDetails.Status!.Value;
-        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, problemDetails.GetType(), cancellationToken: cancellationToken);
         return true;
     }
 
-    private static ProblemDetails GetProblemDetails(HttpContext httpContext, Exception exception)
+    private static ProblemDetails GetProblemDetails(Exception exception)
         => exception switch
         {
             BadRequestException ex => new HttpValidationProblemDetails(ex.Errors ?? [])
             {
-                Status = (int)ex.StatusCode, Title = ex.Message, Instance = httpContext.Request.Path
+                Status = (int)ex.StatusCode,
+                Title = ex.StatusCode.ToString(),
+                Detail = ex.Message
             },
             BoardException ex => new ProblemDetails
             {
-                Status = (int)ex.StatusCode, Title = ex.Message, Instance = httpContext.Request.Path
+                Status = (int)ex.StatusCode,
+                Title = ex.StatusCode.ToString(),
+                Detail = ex.Message
             },
             _ => new ProblemDetails
             {
                 Status = StatusCodes.Status500InternalServerError,
-                Title = InternalServerErrorMessage,
-                Instance = httpContext.Request.Path
+                Title = nameof(HttpStatusCode.InternalServerError),
+                Detail = InternalServerErrorMessage
             }
         };
 }
