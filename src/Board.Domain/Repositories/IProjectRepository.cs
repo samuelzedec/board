5ffd91ab520639ef89f62d5440677d5c04bedf99
Vinicius <viniciuslsilveira.1201@gmail.com>
2026-05-31 15:42:53 -0400

    feat: implementa métodos para persistência

--- src/Board.Domain/Repositories/IProjectRepository.cs
+++ src/Board.Domain/Repositories/IProjectRepository.cs
@@ -2,4 +2,11 @@ using Board.Domain.Entities;
 
 namespace Board.Domain.Repositories;
 
-public interface IProjectRepository : IRepository<Project>;
+public interface IProjectRepository : IRepository<Project>
+{
+    Task<Project?> GetByNameAndOwnerAsync(
+        string name,
+        Guid ownerId,
+        CancellationToken cancellationToken = default);
+    Task<Project[]> GetByOwnerAsync(Guid ownerId, CancellationToken cancellationToken = default);
+}
