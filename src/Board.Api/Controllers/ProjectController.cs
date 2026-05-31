72c9de906aa669e2ba8874f144a5801528e3b586
Vinicius <viniciuslsilveira.1201@gmail.com>
2026-05-31 15:44:34 -0400

    feat: adiciona ProjectController com endpoint para criar projeto

---
+++ src/Board.Api/Controllers/ProjectController.cs
@@ -0,0 +1,23 @@
+﻿using Board.Application.Features.Projects.CreateProject;
+using Mediator;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Board.Api.Controllers;
+
+[ApiController]
+[Route("api/projects")]
+public sealed class ProjectController(ISender sender) : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType<CreateProjectResponse>(StatusCodes.Status201Created)]
+    [ProducesResponseType<ProblemDetails>(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType<ProblemDetails>(StatusCodes.Status404NotFound)]
+    [ProducesResponseType<ProblemDetails>(StatusCodes.Status409Conflict)]
+    public async Task<IActionResult> CreateAsync(
+        CreateProjectCommand command,
+        CancellationToken cancellationToken)
+    {
+        var response = await sender.Send(command, cancellationToken);
+        return Created($"api/projects/{response.Id}", response);
+    }
+}
