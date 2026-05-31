8fbe381935255a49ca8d0fda3e33353a093c5bcf
Samuel Zedec <samuel.ribeiro77f@gmail.com>
2026-05-31 13:16:45 -0400

    docs: adiciona summaries em IPasswordHasher e IUserRepository

--- src/Board.Application/Abstractions/IPasswordHasher.cs
+++ src/Board.Application/Abstractions/IPasswordHasher.cs
@@ -1,7 +1,22 @@
 namespace Board.Application.Abstractions;
 
+/// <summary>
+/// Define a funcionalidade para geração e verificação de hashes de senhas.
+/// </summary>
 public interface IPasswordHasher
 {
+    /// <summary>
+    /// Gera um hash para a senha fornecida.
+    /// </summary>
+    /// <param name="password">A senha em texto puro que será transformada em hash.</param>
+    /// <returns>Uma string representando o hash da senha fornecida.</returns>
     string Hash(string password);
+
+    /// <summary>
+    /// Verifica se a senha fornecida corresponde ao hash da senha armazenado.
+    /// </summary>
+    /// <param name="password">A senha em texto puro que será verificada.</param>
+    /// <param name="passwordHash">O hash da senha armazenado para comparação.</param>
+    /// <returns>Um valor booleano indicando se a senha fornecida corresponde ao hash armazenado.</returns>
     bool Verify(string password, string passwordHash);
 }
--- src/Board.Domain/Repositories/IUserRepository.cs
+++ src/Board.Domain/Repositories/IUserRepository.cs
@@ -4,6 +4,28 @@ namespace Board.Domain.Repositories;
 
 public interface IUserRepository : IRepository<User>
 {
+    /// <summary>
+    /// Obtém um usuário pelo endereço de e-mail fornecido.
+    /// </summary>
+    /// <param name="email">O endereço de e-mail do usuário a ser buscado.</param>
+    /// <param name="cancellationToken">
+    /// Token de cancelamento que pode ser usado para cancelar a operação.
+    /// </param>
+    /// <returns>
+    /// Uma instância de <see cref="User"/> correspondente ao endereço de e-mail fornecido,
+    /// ou null se nenhum usuário for encontrado.
+    /// </returns>
     Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Obtém todos os usuários cadastrados no sistema.
+    /// </summary>
+    /// <param name="cancellationToken">
+    /// Token de cancelamento que pode ser usado para cancelar a operação.
+    /// </param>
+    /// <returns>
+    /// Uma lista somente leitura contendo instâncias de <see cref="User"/>.
+    /// Retorna uma lista vazia se nenhum usuário for encontrado.
+    /// </returns>
     Task<IReadOnlyList<User>> GetAllAsync(CancellationToken cancellationToken = default);
 }
